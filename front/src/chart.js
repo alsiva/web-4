e369513fa12c5fc15731a04ef38ef3b1f8a15574
Alex Ivanov <alexivanovrus@gmail.com>
2021-12-28 22:36:52 +0300

    change chart from radius

--- front/src/chart.js
+++ front/src/chart.js
@@ -2,16 +2,14 @@ import {useDispatch, useSelector} from "react-redux";
 import {useRef} from "react";
 import {addPoint} from "./area";
 
-const chartRadius = 140;
 const size = 400
 const axisSize = 175
 const arrowLength = 15
 const arrowWidth = 8
-const spinnerRadius = chartRadius/4;
-
+const scale = 30;
 
 function getChartCoordinate(value) {
-    return Math.round(100 * value / chartRadius) / 100
+    return Math.round(100 * value / scale) / 100
 }
 
 export function Chart() {
@@ -20,6 +18,9 @@ export function Chart() {
     const dispatch = useDispatch()
     const r = useSelector(state => state.area.form.r.value)
     const isLoading = useSelector(state => state.area.isLoading)
+
+    const scaledRadius = r * scale
+    const spinnerRadius = scaledRadius / 4;
 
     return (
         <svg
@@ -49,21 +50,15 @@ export function Chart() {
             transform="scale(1, -1)"
         >
             <g fill="#09b0e8">
-                <rect x="0" y={-chartRadius} width={chartRadius/2} height={chartRadius}/>
-                <polygon points={`0,0 0,${-chartRadius} -${chartRadius},0`} />
-                <path d={`M 0 ${chartRadius/2} A ${chartRadius/2} ${chartRadius/2} 0 0 1 ${-chartRadius/2} 0 L 0 0`}/>
+                <rect x="0" y={-scaledRadius} width={scaledRadius/2} height={scaledRadius}/>
+                <polygon points={`0,0 0,${-scaledRadius} -${scaledRadius},0`} />
+                <path d={`M 0 ${scaledRadius/2} A ${scaledRadius/2} ${scaledRadius/2} 0 0 1 ${-scaledRadius/2} 0 L 0 0`}/>
             </g>
 
 
             <line x1="0" x2="0" y1={-axisSize} y2={axisSize} stroke="black" strokeWidth="1"/>
             <line x1={-axisSize} x2={axisSize} y1="0" y2="0" stroke="black" strokeWidth="1"/>
 
-            {[0, 90, 180, 270].map(angle => (
-                <g key={angle} transform={`rotate(${angle} 0 0)`}>
-                    <line x1="-5" x2="5" y1={`${chartRadius/2}`} y2={`${chartRadius/2}`} stroke="black" strokeWidth="1"/>
-                    <line x1="-5" x2="5" y1={`${chartRadius}`} y2={`${chartRadius}`} stroke="black" strokeWidth="1"/>
-                </g>
-            ))}
             {[0, 270].map(angle => (
                 <polygon
                     key={angle}
@@ -73,19 +68,23 @@ export function Chart() {
             ))}
 
             <g transform="scale(1, -1)">
-                <text x="5" y={`${chartRadius/2}`}>-R/2</text>
-                <text x="5" y={`${chartRadius}`}>-R</text>
-                <text x="10" y={`${-chartRadius/2}`}>R/2</text>
-                <text x="10" y={`${-chartRadius}`}>R</text>
+                {[-5, -4, -3, -2, -1, 1, 2, 3, 4, 5].map(i => {
+                    let scaledI = i * scale;
 
-                <text x={`${-chartRadius}`} y="-10" textAnchor="middle">-R</text>
-                <text x={`${-chartRadius/2}`} y="-10" textAnchor="middle">-R/2</text>
-                <text x={`${chartRadius/2}`} y="-10" textAnchor="middle">R/2</text>
-                <text x={`${chartRadius}`} y="-10" textAnchor="middle">R</text>
+                    return (
+                        <g key={i}>
+                            <text x="-10" y={scaledI} alignmentBaseline="middle" textAnchor="end">{-i}</text>
+                            <line x1="-5" x2="5" y1={scaledI} y2={scaledI} stroke="black" strokeWidth="1" />
+
+                            <text x={scaledI + 4} y="-10" textAnchor="end">{i}</text>
+                            <line x1={scaledI} x2={scaledI} y1="-5" y2="5" stroke="black" strokeWidth="1" />
+                        </g>
+                    );
+                })}
             </g>
 
             {hits.map(hit => {
-                const ratio = chartRadius / hit.r
+                const ratio = scaledRadius / hit.r
 
                 return (
                     <circle
